801f49cf7d6d14bb6b8a5d8432c813af6d09f023
Eshan <2016mehrab@gmail.com>
2025-05-19 12:42:37 +0600

    feat(project): add skeleton for Project module
    - Created `ProjectController` with resource methods
    - Created `Project` model with migration
    - Added placeholder views for Project (index, create, etc.)
    - Added resource routes in `web.php`
    - Set up basic CRUD structure (not fully implemented yet)

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
+    protected $fillable = [
+        "title", 
+        "description",
+        "image_path",
+        "project_url",
+        "status"
+    ];
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
       @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <body>
+    <div class="d-flex flex-column min-vh-100">
       <header class="bg-primary text-white shadow">
           <div class="container py-3">
               <nav class="navbar navbar-expand-lg">
@@ -18,10 +19,10 @@
                       <div class="collapse navbar-collapse" id="navbarNav">
                           <ul class="navbar-nav ms-auto">
                               <li class="nav-item">
-                                  <a  href="{{ url('/') }}">Projects</a>
+                                  <a class="nav-link" href="{{route('project.index')}}">Projects</a>
                               </li>
                               <li class="nav-item">
-                                <a href="">Create Project</a>
+                                <a href="{{route('project.create')}}" class="nav-link">Create</a>
                               </li>
                           </ul>
                       </div>
@@ -30,7 +31,7 @@
           </div>
       </header>
 
-      <main class="container py-4">
+      <main class="container py-4 flex-grow-1">
           @if (session('success'))
               <div class="alert alert-success">
                   {{ session('success') }}
@@ -42,5 +43,7 @@
       <footer class="bg-dark text-white text-center py-3">
           <p>Made by Eshan</p>
       </footer>
+
+    </div>
   </body>
   </html>
